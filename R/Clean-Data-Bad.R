794a7563a41ef82b61529c0111586c4917ee98dc
Danielle Hawk <daniellehawk422@gmail.com>
2025-08-18 12:03:02 -0400

    Altered the code to agree with a txt file type. Ensured setwd and getwd responses matched

--- R/Clean-Data-Bad.R
+++ R/Clean-Data-Bad.R
@@ -1,19 +1,18 @@
 install.packages("dplyr", "tidyverse")
-library(dplyr)
-library(tidyverse)
+
 nlsy_cols <- c("glasses", "eyesight", "sleep_wkdy", "sleep_wknd",
 							 "id", "nsibs", "samp", "race_eth", "sex", "region",
 							 "income", "res_1980", "res_2002", "age_bir")
 
 library(tidyverse)
 setwd("C:/Users/dano7/OneDrive/Documents/Epi590R-ClassActivity/Data/Raw")
-nlsy <- read_csv("nlsy",
+nlsy <- read_table("nlsy.txt",
 								 na = c("-1", "-2", "-3", "-4", "-5", "-998"),
 								 skip = 1, col_names = nlsy_cols)
 
 library(dplyr)
 nlsy <- nlsy |>
-	mutate(region_cat = factor(region, labels = c("Northeast", "North Central", "South", "West")),
+	mutate (region_cat = factor (region, labels = c("Northeast", "North Central", "South", "West")),
 				 sex_cat = factor(sex, labels = c("Male", "Female")),
 				 race_eth_cat = factor(race_eth, labels = c("Hispanic", "Black", "Non-Black, Non-Hispanic")),
 				 eyesight_cat = factor(eyesight, labels = c("Excellent", "Very good", "Good", "Fair", "Poor")),
@@ -23,3 +22,4 @@ nlsy <- na.omit(nlsy)
 
 setwd("../clean/")
 write_rds(nlsy, "nlsy-complete-cases.rds")
+
